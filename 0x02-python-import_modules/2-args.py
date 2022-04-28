b550e88298f260f84f02e2a7c557697cc271c38f
KristiSeraj <kristiseraj2@gmail.com>
2022-04-28 03:47:05 -0700

    List of arguments in Python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
-n = len(sys.argv)
-if n == 1:
-    print('0 argument.')
-else:
-    print('{} argument:'.format(n - 1))
-for i in range(1, n):
-    print('{}: {}'.format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv)
+    if n == 1:
+        print('0 argument.')
+    else:
+        print('{} argument:'.format(n - 1))
+    for i in range(1, n):
+        print('{}: {}'.format(i, sys.argv[i]))
