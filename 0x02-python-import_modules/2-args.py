e8abf57ad180740f21aca7fc1f26c111daa0c84a
KristiSeraj <kristiseraj2@gmail.com>
2022-04-28 03:53:09 -0700

    Argument list in Python fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    n = len(sys.argv)
-    if n == 1:
-        print('0 argument.')
+    n = len(sys.argv) - 1
+    if n == 0:
+        print('0 arguments.')
+    elif n == 1:
+        print('1 argument:')
     else:
-        print('{} argument:'.format(n - 1))
-    for i in range(1, n):
-        print('{}: {}'.format(i, sys.argv[i]))
+        print('{} argument:'.format(n))
+    for i in range(n):
+        print('{}: {}'.format(i + 1, sys.argv[i + 1]))
