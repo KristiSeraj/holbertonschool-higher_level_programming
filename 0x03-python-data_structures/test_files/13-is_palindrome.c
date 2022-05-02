dbd062a8edef895077ac812fb4dd72d9f56fcf3f
KristiSeraj <kristiseraj2@gmail.com>
2022-05-02 12:20:13 -0700

    Delete at index

---
+++ 0x03-python-data_structures/test_files/13-is_palindrome.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * is_palindrome - check if a singly linked list is a palindrome
+ * @head: head of the linked list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+
+}
