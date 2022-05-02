a2c99f680a730e607f313eaac8b21ce6b8571c61
KristiSeraj <kristiseraj2@gmail.com>
2022-05-02 09:59:34 -0700

    fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == "":
-        sentence[0] = None
+        sentence[0] = "None"
     sentence_tuple = (len(sentence), sentence[0])
     return sentence_tuple
