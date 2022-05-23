0260c301d7870df74795229653c47e2f34ccb182
KristiSeraj <kristiseraj2@gmail.com>
2022-05-23 10:40:55 -0700

    doc string fixed

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle:
     Attributes:
         number_of_instances (int): shows the number of instances
         created and deleted
-        print_symbol (int): symbol for string representation
+        print_symbol (any type): symbol for string representation
     """
 
     number_of_instances = 0
