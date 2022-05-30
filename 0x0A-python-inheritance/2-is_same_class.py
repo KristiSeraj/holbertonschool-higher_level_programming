e76056a89b49f402bcb148c83ace8a15e45b93ba
KristiSeraj <kristiseraj2@gmail.com>
2022-05-30 03:40:25 -0700

    new files added

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Defines a module with boolean values"""
+
+
+def is_same_class(obj, a_class):
+    """Returns True if the object is exactly an instance
+    of the specific class, otherwise False"""
+
+    return type(obj) is a_class
