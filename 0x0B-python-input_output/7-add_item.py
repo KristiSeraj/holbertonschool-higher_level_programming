e060fd963d3202b31436824034edac9a00e54529
KristiSeraj <serajkristi@gmail.com>
2022-05-31 06:17:15 -0700

    Add all arguments to list

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+import os.path
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+argList = []
+if os.path.exists("add_item.json"):
+    argList = load_from_json_file("add_item.json")
+save_to_json_file(argList + sys.argv[1:], "add_item.json")
