11a07d6d02b698d294974b8f37cfb70096add2d8
KristiSeraj <serajkristi@gmail.com>
2022-06-02 05:45:28 -0700

    Square dictionary representation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -38,6 +38,11 @@ class Square(Rectangle):
                 if hasattr(self, key):
                     setattr(self, key, value)
 
+    def to_dictionary(self):
+        """Returns the dictionary representation"""
+
+        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
+
     def __str__(self):
         """Str representation of square"""
 
