3cfca7d799f1cf77bc2a801dd50d3ab1219382d3
KristiSeraj <serajkristi@gmail.com>
2022-06-07 01:00:21 -0700

    new test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -11,3 +11,14 @@ class TestBase(unittest.TestCase):
         base1 = Base()
         base2 = Base()
         self.assertEqual(base1.id, base2.id - 1)
+
+    def test_none_id(self):
+        base1 = Base(None)
+        base2 = Base(None)
+        self.assertEqual(base1.id, base2.id - 1)
+
+    def test_three_base(self):
+        base1 = Base()
+        base2 = Base()
+        base3 = Base()
+        self.assertEqual(base1.id, base3.id - 2)
