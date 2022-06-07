b8a093ddc4aa5ca4e4d7d02861cbae5102d89876
KristiSeraj <serajkristi@gmail.com>
2022-06-07 04:39:04 -0700

    load from file

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -110,3 +110,47 @@ class TestBaseSaveToFile(unittest.TestCase):
         Square.save_to_file(None)
         with open("Square.json", "r") as f:
             self.assertEqual("[]", f.read())
+
+class TestBaseLoadFrom(unittest.TestCase):
+    # Delete created files
+    @classmethod
+    def tearDown(self):
+        try:
+            os.remove("Rectangle.json")
+        except IOError:
+            pass
+        try:
+            os.remove("Square.json")
+        except IOError:
+            pass
+
+    def test_load_from_file_first_rect(self):
+        rect1 = Rectangle(3, 4, 5, 6, 1)
+        rect2 = Rectangle(7, 8, 9, 10, 2)
+        Rectangle.save_to_file([rect1, rect2])
+        list_rect = Rectangle.load_from_file()
+        self.assertEqual(str(rect1), str(list_rect[0]))
+
+    def test_load_from_file_second_rect(self):
+        rect1 = Rectangle(3, 4, 5, 6, 1)
+        rect2 = Rectangle(7, 8, 9, 10, 2)
+        Rectangle.save_to_file([rect1, rect2])
+        list_rect = Rectangle.load_from_file()
+        self.assertEqual(str(rect2), str(list_rect[1]))
+
+    def test_load_from_file_first_sq(self):
+        sq1 = Square(3, 4, 5, 1)
+        sq2 = Square(6, 7, 8, 2)
+        Square.save_to_file([sq1, sq2])
+        list_sq = Square.load_from_file()
+        self.assertEqual(str(sq1), str(list_sq[0]))
+
+    def test_load_from_file_second_sq(self):
+        sq1 = Square(3, 4, 5, 1)
+        sq2 = Square(6, 7, 8, 2)
+        Square.save_to_file([sq1, sq2])
+        list_sq = Square.load_from_file()
+        self.assertEqual(str(sq2), str(list_sq[1]))
+
+if __name__ == "__main__":
+    unittest.main()
