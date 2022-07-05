c97d1f591925aad5c6c460b8bc13e601bfe93af8
KristiSeraj <serajkristi@gmail.com>
2022-07-05 05:08:38 -0700

    comment changed for 11-best_score.sql, added task 12-no_cheating.sql and 13-change_class.sql which both modifies records

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
--- List all records with a score under 10 in the second_table
+-- List all records with a score >= 10 in the second_table
 SELECT `score`, `name` FROM `second_table` WHERE `score` >= 10 ORDER BY `score` DESC;
