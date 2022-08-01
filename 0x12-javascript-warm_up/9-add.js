bb5ce8796546e7d0534a5ec8f626d468534380c2
KristiSeraj <serajkristi@gmail.com>
2022-08-01 05:14:59 -0700

    Fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 function add (a, b) {
-  if (a === undefined || b === undefined) {
-    console.log('NaN');
-  } else {
-    return parseInt(a) + parseInt(b);
-  }
+  return parseInt(a) + parseInt(b);
 }
 const num1 = process.argv[2];
 const num2 = process.argv[3];
