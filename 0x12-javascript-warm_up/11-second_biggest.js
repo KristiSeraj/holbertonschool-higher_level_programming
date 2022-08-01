e5f46ba1c944496c348a09a7851e19e2d2169eec
KristiSeraj <serajkristi@gmail.com>
2022-08-01 06:06:34 -0700

    fix:

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,6 @@ const listArg = process.argv.slice(2);
 if (listArg.length === 0 || listArg.length === 1) {
   console.log(0);
 } else {
-  listArg.sort();
-  listArg.pop();
-  console.log(listArg[listArg.length - 1]);
+  listArg.sort((a, b) => a - b);
+  console.log(listArg[listArg.length - 2]);
 }
