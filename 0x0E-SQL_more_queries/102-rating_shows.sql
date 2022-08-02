7cc9914aac62d75f0eb21d494d47c4c74c559f3d
KristiSeraj <serajkristi@gmail.com>
2022-08-02 12:44:31 -0700

    changes

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -2,6 +2,6 @@
 SELECT tv_shows.title, SUM(rate) AS rating
 FROM tv_shows 
 INNER JOIN tv_show_ratings 
-ON tv_show_ratings.show_id = tv_shows.id 
+  ON tv_show_ratings.show_id = tv_shows.id 
 GROUP BY tv_shows.title 
 ORDER BY rating DESC;
