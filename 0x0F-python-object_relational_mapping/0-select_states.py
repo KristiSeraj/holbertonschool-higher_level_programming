b01aba3470baea426d98a33c0324915157eb2092
KristiSeraj <serajkristi@gmail.com>
2022-08-04 14:38:56 -0700

    pycodestyle fixed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,8 +6,9 @@ import sys
 
 if __name__ == '__main__':
 
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], charset="utf8")
     cr = db.cursor()
     cr.execute("SELECT * FROM states ORDER BY states.id")
     res = cr.fetchall()
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,8 +8,9 @@ import sys
 
 if __name__ == '__main__':
 
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], charset="utf8")
     cr = db.cursor()
     myQuery = "SELECT * FROM states WHERE name=%(name)s ORDER BY states.id"
     cr.execute(myQuery, {'name': sys.argv[4]})
