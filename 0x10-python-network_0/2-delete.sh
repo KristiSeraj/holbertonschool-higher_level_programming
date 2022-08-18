4d2535305aaa6f06f242cd3fe96249a7f25b4f7b
KristiSeraj <serajkristi@gmail.com>
2022-08-18 05:30:08 -0700

    delete method

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Displays only body of a 200 status code response
+# Sends a DELETE request to the URL passed as the first argument
+# and displays the body of the response
 curl -sX DELETE "$1"
