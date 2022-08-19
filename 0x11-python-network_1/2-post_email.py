93ae4cc4675ff97621db58d689ab072d22ff24d4
KristiSeraj <serajkristi@gmail.com>
2022-08-19 05:02:32 -0700

    fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,10 +4,10 @@ import urllib.request
 import urllib.parse
 from sys import argv
 if __name__ == "__main__":
-    value = {"email": argv[2]}
+    value = {'email': argv[2]}
     data = urllib.parse.urlencode(value)
     data = data.encode('ascii')
     req = urllib.request.Request(argv[1], data)
     with urllib.request.urlopen(req) as response:
         page = response.read()
-        print("Your email is: {}".format(argv[2]))
+        print("{}".format(page.decode('utf8')))
