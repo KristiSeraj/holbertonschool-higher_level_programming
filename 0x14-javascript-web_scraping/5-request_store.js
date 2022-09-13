12ebd3edee6bc76e98276f82ec051be969a80f4e
KristiSeraj <serajkristi@gmail.com>
2022-09-13 06:13:36 -0700

    fix semistandard

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,9 +6,9 @@ const fs = require('fs');
 axios.get(process.argv[2])
   .then(function (response) {
     fs.writeFile(process.argv[3], response.data, err => {
-	    if (err) {
-		    console.log(err);
-	    }
+      if (err) {
+        console.log(err);
+      }
     });
   })
   .catch(function (error) {
